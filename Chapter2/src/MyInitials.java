576f23fc4ab0a7edbbdace2b4ab265eb9092abdd
16365506 <16365506@L815WD220032.edu.cbe.ab.ca>
2022-09-12 11:49:46 -0600

    9/12 11:49

---
+++ Chapter2/src/MyInitials.java
@@ -0,0 +1,21 @@
+
+public class MyInitials 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("           /\\                 |     /\n"
+				+ "          /  \\                |    /\n"
+				+ "         /    \\               |   /\n"
+				+ "        /      \\              |  /\n"
+				+ "       /        \\             | /\n"
+				+ "      /__________\\            |/\n"
+				+ "     /            \\           |\\\n"
+				+ "    /              \\          | \\\n"
+				+ "   /                \\         |  \\\n"
+				+ "  /                  \\        |   \\\n"
+				+ " /                    \\       |    \\\n"
+				+ "/                      \\      |     \\\n\n\n\n");
+	}
+
+}
